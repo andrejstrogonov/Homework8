3872bd3a622f5bd9966b671ee2d960da61f63fb2
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-21 22:13:29 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int [] firstWeight = new int [3];
+        firstWeight[0] = 1;
+        firstWeight[1] = 2;
+        firstWeight[2] = 3;
+
+        float [] secondWeight = {1,57,7,654, 9,986};
+
+
+
     }
 }
