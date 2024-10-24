3583560242156a81aaf9c1e60cee4d93fac97e1d
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-24 18:49:20 +0300

    Start refactoring

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //1 задача
+        //1 массив
         int [] firstWeight = new int [3];
         firstWeight[0] = 1;
         firstWeight[1] = 2;
         firstWeight[2] = 3;
+        // 2 массив
+        float [] secondWeight = {1.57f,7.654f, 9.986f};
+        // 3 массив
+        long [] thirdWeight = new long [5];
+        thirdWeight[0]= 45635776765767L;
+        thirdWeight[1]= 7635657367657367L;
+        thirdWeight[2]= 3365767653L;
+        thirdWeight[3]= 56737675L;
+        thirdWeight[4]= 76675353365L;
+
+
+
         String firstWeightString = Arrays.toString(firstWeight);
         System.out.print(Arrays.toString(firstWeight));
         for (int i = 0; i < firstWeight.length; i++) {
@@ -29,7 +43,7 @@ public class Main {
 
 
         System.out.println();
-        float [] secondWeight = {1.57f,7.654f, 9.986f};
+
 
         float[] reversedSecondWeight = new float[secondWeight.length];
         for (int i = 0; i < secondWeight.length; i++) {
@@ -37,12 +51,7 @@ public class Main {
         }
         System.out.println(Arrays.toString(reversedSecondWeight));
 
-        long [] thirdWeight = new long [5];
-        thirdWeight[0]= 45635776765767L;
-        thirdWeight[1]= 7635657367657367L;
-        thirdWeight[2]= 3365767653L;
-        thirdWeight[3]= 56737675L;
-        thirdWeight[4]= 76675353365L;
+
         long[] reversedThirdWeight = new long[thirdWeight.length];
         for (int i = 0; i < thirdWeight.length; i++) {
             reversedThirdWeight[i] = thirdWeight[thirdWeight.length - 1 - i];
